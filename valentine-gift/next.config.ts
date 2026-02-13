b411cfdf252f6a3a5b2d0db0945bf72c4a37c33c
SgaxaMabhande <sikhumbuzobembe184@gmail.com>
2026-02-13 21:26:40 +0200

    added new config

--- valentine-gift/next.config.ts
+++ valentine-gift/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',        // enable static HTML export
+    reactStrictMode: true,   // optional but recommended
 };
 
 export default nextConfig;
